691baa23fb95977ae380efc293a88063f85643aa
Christopher Ly <demedial@gmail.com>
2018-03-12 16:37:41 +0000

    Created and passed link spec test

--- app.rb
+++ app.rb
@@ -1,9 +1,12 @@
-require 'sinatra'
+require 'sinatra/base'
+require './lib/link'
 
 class BookmarkManager < Sinatra::Base
-
   get '/' do
-    "Hello World"
+    @links = Link.all
+    erb :index
   end
+
+  run! if app_file == $0
 
 end
