a100d316886fe21b71a69d88be45e83a0ced961b
Christopher Ly <demedial@gmail.com>
2018-03-13 15:20:13 +0000

    Added test environment for databases and script for setting up the test db

--- lib/link.rb
+++ lib/link.rb
@@ -2,7 +2,12 @@ require 'pg'
 
 class Link
   def self.all
-    connection = PG.connect :dbname => 'bookmark_manager', :user => 'christopher'
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect :dbname => 'bookmark_manager_test'
+    else
+      connection = PG.connect :dbname => 'bookmark_manager'
+    end
+
     result = connection.exec "SELECT url FROM links"
     result.map { |link| link['url'] }
   end
