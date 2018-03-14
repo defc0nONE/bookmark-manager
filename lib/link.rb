ffa755ee2e7aa704a155c39fdb3dc29d795c2539
Christopher Ly <demedial@gmail.com>
2018-03-14 22:37:09 +0000

    Added @id as an instance variable for Link, modified .all method and index.erb to accommodate

--- lib/link.rb
+++ lib/link.rb
@@ -1,15 +1,16 @@
 require './lib/database_connection'
 
 class Link
-  attr_reader :url
+  attr_reader :id, :url
 
-  def initialize(url)
+  def initialize(id, url)
+    @id = id
     @url = url
   end
 
   def self.all
     result = DatabaseConnection.query "SELECT * FROM links"
-    result.map { |link| Link.new(link['url']) }
+    result.map { |link| Link.new(link['id'], link['url']) }
   end
 
   def self.add(new_url)
